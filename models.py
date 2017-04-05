f78b1bb1b17ff13f2faa99f976f463e8c61f156f
Saniiit <sanjanagunna@gmail.com>
2017-04-05 17:30:50 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,40 +1,30 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
 
-#app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test3.db'
-#db = SQLAlchemy(app)
-class User(db.Model):
-    __tablename__ = 'user_table'
+class Transaction(db.Model):
+    __tablename__ = 'transaction_table'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
-    bills = db.Column(db.Integer)
-    balance = db.Column(db.Integer)
+    description = db.Column(db.String(255))
+    amount = db.Column(db.Integer)
+    majorSplit = db.Column(db.String(255))
+    split_amongst = db.Column(db.String(255))
 
-    def __init__(self, name, email, password, bills, balance):
-        self.name = name
-        self.email = email
-        self.password = generate_password_hash(password)
-        self.bills = bills
-        self.balance = balance
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    def __init__(self, description, amount, majorSplit, split_amongst):
+        self.description = description
+        self.amount = amount
+        self.majorSplit = majorSplit
+        self.split_amongst = split_amongst
 
     def to_dict(self):
         return {
-            'id' : self.id,
-            'name': self.name,
-            'email': self.email,
-            'bills' : self.bills,
-            'balance' : self.balance,
+            'id': self.id,
+            'description': self.description,
+            'amount': self.amount,
+            'majorSplit': self.majorSplit,
+            'split_amongst': self.split_amongst,
         }
 
     def __repr__(self):
-        return "User<%d> :%s :: %s,Balance :%s" %(self.id, self.name,self.email,self.balance)
+        return "Transaction<%d>: %s;Amount:%s;%s,%s" %(self.id, self.description,self.amount,self.majorSplit,self.split_amongst)
 
 db.create_all()
